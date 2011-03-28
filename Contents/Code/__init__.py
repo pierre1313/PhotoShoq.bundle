334567f601740a08931bada5e8fc6779494b5ed3
Pierre <p.dellanave@gmail.com>
2011-03-28 01:07:59 -0700

    Converted to v2 - updated RSS feed address

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,23 +1,20 @@
-from PMS import Plugin, Log, DB, Thread, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import MediaContainer, DirectoryItem, PhotoItem
-
 PLUGIN_PREFIX   = "/photos/PhotoShoq"
-RSS_FEED        = "http://photo.shoq.com/index.php?PageID=108"
+#RSS_FEED        = "http://photo.shoq.com/index.php?PageID=108"
+RSS_FEED        = "http://jaspervandermeij.nl/feed/"
 
 ####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "Photo.Shoq", "icon-default.png", "art-default.png")
-  Plugin.AddViewGroup("Pictures", viewMode="InfoList", contentType="photos")
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "Photo.Shoq", "icon-default.png", "art-default.png")
+  Plugin.AddViewGroup("Pictures", viewMode="InfoList", mediaType="photos")
 
 ####################################################################################################
-def HandlePhotosRequest(pathNouns, count):
-  dir = MediaContainer("art-default.png", "Pictures", "Photo.Shoq")
+def MainMenu():
+  dir = MediaContainer(art=R("art-default.png"), viewGroup ="Pictures", title1="Photo.Shoq")
   
-  if count == 0:
-    for item in RSS.Parse(RSS_FEED).entries:
-      node = XML.ElementFromString(item.content[0].value, True)
-      summary = ' '.join(node.xpath("//text()")).replace('\n','').strip()
-      img = node.xpath("//img")[0].get('src')
-      dir.AppendItem(PhotoItem(img, item.title, summary, img))
+  for item in RSS.FeedFromURL(RSS_FEED).entries:
+    node = HTML.ElementFromString(item.content[0].value)
+    summary = ' '.join(node.xpath("//text()")).replace('\n','').strip()
+    img = node.xpath("//img")[0].get('src')
+    dir.Append(PhotoItem(img, item.title, summary, img))
 
-  return dir.ToXML()
+  return dir
